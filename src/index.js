aae4a2b18f1a0f2d94bb8ff5d5087fd18d69dccf
visheshtomarr <visheshtomar7800@gmail.com>
2025-04-29 19:06:30 +0530

    Added some styling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
     {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
     return (
-        <div>
+        <div className="container">
             <Header />
             <Menu />
             <Footer />
@@ -57,27 +58,42 @@ function App() {
 }
 
 function Header() {
-    return <h1>Fast React Pizza Co.</h1>
+    return (
+        <header className="header">
+            <h1>
+                Fast React Pizza Co.
+            </h1>
+        </header>
+    );
 }
 
 function Menu() {
     return (
-        <div>
+        <main className="menu">
             <h2>Our Menu</h2>
             <Pizza />
-        </div>
-    )
+        </main>
+    );
 }
 
 function Footer() {
-    return <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
+    const hours = new Date().getHours();
+    const openHrs = 12;
+    const closeHrs = 22;
+    const isOpen = hours >= openHrs && hours <= closeHrs;
+    console.log(isOpen);
+    return (
+        <footer className="footer">
+            {new Date().toLocaleTimeString()} We're currently open!
+        </footer>
+    );
 }
 
 function Pizza() {
     return (
         <div>
             <img src="pizzas/spinaci.jpg" alt="Pizza" />
-            <h2>Pizza Spinaci</h2>
+            <h3>Pizza Spinaci</h3>
             <p>Tomato, mozarella, spinach, and ricotta cheese</p>
         </div>
     );
